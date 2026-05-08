c30d00f57551a7d220d091d6dfa910b0871804b4
nttc5405-a11y <nttc5405@gmail.com>
2026-05-08 20:16:58 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+// 繩索受力分析系統 - Service Worker
+const CACHE = 'rope-analysis-v1';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.svg'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(e.request)
+        .then((resp) => {
+          // 只快取同源的成功回應
+          if (resp && resp.ok && new URL(e.request.url).origin === location.origin) {
+            const clone = resp.clone();
+            caches.open(CACHE).then((c) => c.put(e.request, clone));
+          }
+          return resp;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
